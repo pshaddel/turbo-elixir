f6656d3a54a32d2fa3db8b6cea22b7631d540a59
Poorshad <p.shaddel@gmail.com>
2021-03-25 19:54:00 +0430

    Using maps in elixir

---
+++ cards/lib/sideTopic.ex
@@ -0,0 +1,23 @@
+defmodule SideTopic do
+  def use_map do
+    ourMap = %{ name: "poorshad", family: "shaddel" }
+    ourMap.name
+  end
+
+  def map_pattern_matching do
+    ourMap = %{ name: "poorshad", family: "shaddel" }
+    %{ name: myName } = ourMap
+    myName
+  end
+
+  def map_update do
+    myMap = %{name: "Poorshad" }
+    myMap = Map.put(myMap, :name, "Hasan")
+    myMap
+  end
+
+  def map_set_new do
+    myMap = %{ name: "Khodadad" }
+    Map.put_new(myMap, :family, "Azizi")
+  end
+end
